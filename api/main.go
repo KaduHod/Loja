b48ab7e853022ae27aa81cae9d811ffbc5a1dbdb
kaduhod <carlosjr.ribas@gmail.com.br>
2024-09-07 18:07:32 -0300

    :construction: Corrigido logs do nginx e adicionado logrotate

--- api/main.go
+++ api/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
     fmt.Println("Hello")
+    server := gin.Default()
+    server.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message":"ok",
+        })
+    })
+    server.Run("0.0.0.0:8080")
 }
